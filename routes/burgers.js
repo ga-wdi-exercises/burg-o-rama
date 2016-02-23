6f70417c94ce6ca404c00e606c799ea7b7d4ca22
Patrick M <Patrick.M.Professional@gmail.com>
2016-02-23 18:01:55 -0500

    exercise completed with out using the array. should be easier to implement with DB when it is up and running.

--- routes/burgers.js
+++ routes/burgers.js
@@ -5,14 +5,21 @@ var burgers = express.Router(); //find out what .ROUTER() does ==> gets routes s
 
 
  // /burgers : to create a burger or list burgers (depending on the HTTP verb used
-burgers.route('/burgers')
- .get( (req,res) => {
-  res.send('/burger done got');
+burgers.route('/')
+  .get( (req,res) => {
+    res.render('pages/burgerGallery.html.ejs');//must include .html.ejs, i don't know why Jason can leave it out. probably is cheating. /haterade
+    // res.send('/burgers done got');
 })
+  .post( /*db.creates,*/ (req,res) => {
+    //form should trigger post request. data should be referenced as req.body.something (name, toppings, etc)->how are these names determined by field name label?  call SQL query function to insert value.
+    //then redirect with
+    //res.redirect('/' + someVarMatchingNewlyAddedBurgerID); //this need to be URL, not route path.
+    res.send('/burgers gone postal');
+  })
 
 
 // /burgers/new : to view the 'new burger' form.
-burgers.route('/burgers/new')
+burgers.route('/new')
   .get( (req,res) => {
     res.send('/burgers/new done got');
   })
@@ -21,24 +28,56 @@ burgers.route('/burgers/new')
   } )
 
 //  /burgers/:id : to view/edit/delete ONE burger.
-burgers.route('/burgers/:id')
+burgers.route('/:burgerID')
   .get( (req,res) => {
-    res.send('/burgers/:id done got');
+    var bID = req.params.burgerID;
+    //confirm burger exists, or spit error.
+    if(!(bID/*in database //not sure how to check for this right now */)){
+      res.sendStatus(404);
+      return;
+    }
+    res.render('pages/burgerSingle.html.ejs', {
+      burgerID: bID
+    })
+    // res.send('/burgers/:id done got');
   })
   .put( (req,res) => {
-    res.send('/burgers/:id done put-ed');
+    var bID = req.params.burgerID;
+    //confirm burger exists, or spit error.
+    if(!(bID/*in database //not sure how to check for this right now */)){
+      res.sendStatus(404);
+      return;
+    }
+    /*
+    call function to update DB for this burger. and ONLY this burger.
+    */
+    console.log('put-ed redirect-ed');
+    // req.method = 'get';//from user alessioalex at http://stackoverflow.com/questions/8240447/express-js-cant-redirect
+    res.redirect(303, '/burgers/'+bID ) //needs to be URL not route path
+    // res.send('/burgers/:burgerID done put-ed');
   })
   .delete( (req,res) => {
-    res.send('/burgers/:id done deleted');
+    var bID = req.params.burgerID;
+    //confirm burger exists, or spit error.
+    if(!(bID/*in database //not sure how to check for this right now */)){
+      console.log('someone tried to delete a burger that did not exist');
+      return;
+    }
+    /*
+    call function to update DB by removing this burger. and ONLY this burger.
+    */
+    console.log('a burger got deleted. was '+bID);
+    res.redirect(303, '/burgers' ) //needs to be URL not route path
+    // res.send('/burgers/:burgerID done deleted');
   })
 
 
 
 
 // /burgers/:id/edit : to view the 'edit burger' form.
-burgers.route('/burgers/:id/edit')
+burgers.route('/:burgerID/edit')
   .get( (req,res) => {
-    res.send('/burgers/:id/edit done got');
+    res.send('/burgers/:burgerID/edit done got');
   } )
 
 module.exports = burgers;
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var path = require('path');
 var ejs = require('ejs');
 var request = require('request');
 
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
 // port settings
 var port = process.env.port || 3000;
 
@@ -17,8 +20,20 @@ var burgerRoutes = require( path.join( __dirname, 'routes/burgers') );
 // app settings
 var app = express();
 
+// parse incoming forms / we'll be reading the form body, but not accepting files, or anything more than text
+app.use( bodyParser.urlencoded({ extended: false }));
+
+// we'll convert everything we receive into json, for our convenience
+app.use( bodyParser.json());
+
+// override with POST having ?_method=XXXX  e.g. If we need to make a PUT, we'll POST to a url appended with ?_method=put
+app.use( methodOverride('_method') );
+
 // logging
 app.use( logger( 'dev' ) );
+
+//view engine
+app.set( 'view engine', 'ejs' );
 
 // server functioning diagnostic
 app.listen( port, () => {
@@ -30,4 +45,4 @@ app.get( '/', (req,res) => {
   res.render('pages/index.html.ejs');
 })
 
-app.use( '/', burgerRoutes );
+app.use( '/burgers', burgerRoutes );
